d99f4323cd5fd03b411d340d5bea565394e5d409
Eduardo Hoyos <edu.hoyos92@gmail.com>
2019-08-22 21:54:04 -0500

    Guards (parte 1)

---
+++ Angular/Clase09/appGuards/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ListadoAlumnoComponent } from './listado-alumno/listado-alumno.component';
+import { NuevoAlumnoComponent } from './nuevo-alumno/nuevo-alumno.component';
+import { EdicionAlumnoComponent } from './edicion-alumno/edicion-alumno.component';
+import { AutenticacionGuard } from './guards/autenticacion.guard';
+
+import { Routes, RouterModule } from "@angular/router"
+import { AutorizacionGuard } from './guards/autorizacion.guard';
+
+const rutas: Routes = [
+  { path: "", component: LoginComponent },
+  {
+    path: "alumno", children: [
+      { path: "", component: ListadoAlumnoComponent },
+      { path: "nuevo", component: NuevoAlumnoComponent, canActivateChild: [AutorizacionGuard] },
+      { path: "edicion", component: EdicionAlumnoComponent }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ListadoAlumnoComponent,
+    NuevoAlumnoComponent,
+    EdicionAlumnoComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(rutas)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
